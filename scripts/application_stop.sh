de1cfab3f4573f01a12954f2f34d5b7ff7c9307d
Olivier BATTINI <olivier@olivierbattini.fr>
2022-10-09 14:51:04 +0200

    CONFIG Modified application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-#Stopping existing node servers
+
+# Stopping existing node servers
 echo "Stopping any existing node servers"
-pkill node
+pidof node
+RESULT=$?
+
+if test $RESULT -eq 0
+then
+    echo "Found node process, stopping node"
+else
+    echo "No node process found"
+    pkill node
+fi
+echo $?
